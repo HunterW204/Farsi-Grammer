a392c9a439f8bd297f15c419c1ddd56b6d9b5d04
Hunter Watts <hunterswatts@gmail.com>
2026-03-09 11:33:53 -0700

    Create data.js for achievements management

---
+++ data.js
@@ -0,0 +1,12 @@
+// data.js
+
+// Sample data structure for achievements
+const achievements = {
+    total: 200,
+    hidden: 20,
+    list: [
+        /* List of achievements could go here */
+    ]
+};
+
+export default achievements;
